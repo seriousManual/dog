0a4a0efbc3cd2236605b3fb6a4e7fcfd84cc6999
RussellSnyder <russell@rustybass.com>
2018-03-03 23:43:39 +0100

    board!

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,43 +5,93 @@ class Board extends Component {
   render() {
 
     const BOARD_SIZE = 550;
-
-    const spacesMap = [
-      [4, 0], [5, 0], [6, 0],
+    var coordScalar = (BOARD_SIZE / 11);
+    var spaceSize = coordScalar / 2.5;
+    var borderWidth = 3;    
+    
+    const playableSpacesMap = [
+      [4, 0], [5, 0], [6, 0, "green"],
       [6, 1], [6, 2], [6, 3], [6, 4],
       [7, 4], [8, 4], [9, 4],      
-      [10, 4], [10, 5], [10, 6],
+      [10, 4], [10, 5], [10, 6, "red"],
       [9, 6], [8, 6], [7, 6], [6, 6],
       [6, 7], [6, 8], [6, 9],
-      [6, 10], [5, 10], [4, 10], 
+      [6, 10], [5, 10], [4, 10, "blue"], 
       [4, 9], [4, 8], [4, 7], [4, 6],
       [3, 6], [2, 6], [1, 6], 
-      [0, 6], [0, 5], [0, 4],
+      [0, 6], [0, 5], [0, 4, "yellow"],
       [1, 4], [2, 4], [3, 4], [4, 4],
       [4, 3], [4, 2], [4, 1],
     ]
 
-    var spaceKey = 0;
-    var coordScalar = (BOARD_SIZE / 11);
-    var spaceSize = coordScalar / 2.5;
-    var borderWidth = 3;    
-    let spaces = spacesMap.map((coord) => {
-        spaceKey++;
+
+
+    var playablespaceKey = 0;
+    let playableSpaces = playableSpacesMap.map((coord) => {
+      playablespaceKey++;
+        var color = coord[2] ? coord[2] : "white"; 
         return <Field 
             x={coord[0] * coordScalar + spaceSize + borderWidth} 
             y={coord[1] * coordScalar + spaceSize + borderWidth} 
             r={spaceSize}
+            color={color}
             borderWidth={borderWidth}
-            key={spaceKey} 
+            key={playablespaceKey} 
         />
     })
+
+    const startSpacesMap = [
+        [0,0,"yellow"],[0,1,"yellow"],[1,0,"yellow"],[1,1,"yellow"],
+        [9,0,"green"],[10,0,"green"],[9,1,"green"],[10,1,"green"],
+        [0,9,"blue"],[0,10,"blue"],[1,9,"blue"],[1,10,"blue"],
+        [9,9,"red"],[9,10,"red"],[10,9,"red"],[10,10,"red"],
+      ]
       
-    console.log(spaces.length)
+      var startSpaceKey = 0;
+      let startSpaces = startSpacesMap.map((coord) => {
+          startSpaceKey++;
+          var color = coord[2]; 
+          return <Field 
+              x={coord[0] * coordScalar + spaceSize + borderWidth} 
+              y={coord[1] * coordScalar + spaceSize + borderWidth} 
+              r={spaceSize}
+              color={color}
+              borderWidth={borderWidth}
+              key={startSpaceKey} 
+          />
+      })
+  
+
+      const homeSpacesMap = [
+        [5,1,"green"],[5,2,"green"],[5,3,"green"],[5,4,"green"],
+        [9,5,"red"],[8,5,"red"],[7,5,"red"],[6,5,"red"],
+        [5,9,"blue"],[5,8,"blue"],[5,7,"blue"],[5,6,"blue"],
+        [1,5,"yellow"],[2,5,"yellow"],[3,5,"yellow"],[4,5,"yellow"],           
+    ]
+
+    var homeSpaceKey = 0;
+    let homeSpaces = homeSpacesMap.map((coord) => {
+        homeSpaceKey++;
+        var color = coord[2]; 
+        return <Field 
+            x={coord[0] * coordScalar + spaceSize + borderWidth} 
+            y={coord[1] * coordScalar + spaceSize + borderWidth} 
+            r={spaceSize * 0.75}
+            color={color}
+            borderWidth={borderWidth}
+            key={startSpaceKey} 
+        />
+    })
+
+          
+
     return (
       <div className="board">
           <svg height={BOARD_SIZE} width={BOARD_SIZE}>
           {/* {this.props.children} */}
-          {spaces}
+          {playableSpaces}
+          {startSpaces}
+          {homeSpaces}
           </svg>
 
       </div>
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Field extends Component {
   render() {
-    let {x, y, r} = this.props;
-    
+    let {x, y, r, color, isHighlighted} = this.props;
+
     return (
-      <circle cx={x} cy={y} r={r} stroke="black" strokeWidth="3" fill="white" />
+      <circle cx={x} cy={y} r={r}
+        className={isHighlighted ? "highlight" : ""}
+        stroke="black" strokeWidth="3" fill={color} />
     );
   }
 }
